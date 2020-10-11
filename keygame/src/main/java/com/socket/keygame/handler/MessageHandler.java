55abe5424f82facc6074a513ddd6ef55cd86bee8
Pradip Khomane <pradipkhomane@gmail.com>
2020-10-11 23:43:15 +0530

    --init

--- keygame/src/main/java/com/socket/keygame/handler/MessageHandler.java
+++
@@ -1,9 +0,0 @@
-package com.socket.keygame.handler;
-
-import javax.websocket.Session;
-
-import com.socket.keygame.model.Message;
-
-public interface MessageHandler {
-	public Message handlerMessage(Message message, Session session);
-}
--- keygame/src/main/java/com/socket/keygame/model/MessageType.java
+++
@@ -1,6 +0,0 @@
-package com.socket.keygame.model;
-
-public enum MessageType {
-	START,
-	QUIT
-}
